9df1a569c34f2b17e20cf6468539253bf835ded5
justinjcl <justinjcornejo@gmail.com>
2025-09-22 01:58:18 -0700

    update sql staging file

--- data/processed/01_create_staging.sql
+++ data/processed/01_create_staging.sql
@@ -12,3 +12,18 @@ desc gdp_staged;
 --
 select * from gdp_staged
 limit 10;
+
+-- create table for cleaning/data exploration 
+create table unemployment_staging 
+like unemployment; 
+
+-- insert data into staging table 
+insert unemployment_staging 
+select * from unemployment; 
+
+-- data types 
+desc unemployment_staging;
+
+-- select first ten rows of data set 
+select * from unemployment_staging 
+limit 10; 
